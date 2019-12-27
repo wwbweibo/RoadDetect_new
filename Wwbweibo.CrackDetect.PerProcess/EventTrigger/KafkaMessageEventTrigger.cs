aa09f00eb00b25b3efaefcbef662dc968997478a
wangweibo <wangweibo@beisen.com>
2019-12-27 11:30:16 +0800

    预处理消息异步处理

--- Wwbweibo.CrackDetect.PerProcess/EventTrigger/KafkaMessageEventTrigger.cs
+++ Wwbweibo.CrackDetect.PerProcess/EventTrigger/KafkaMessageEventTrigger.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Wwbweibo.CrackDetect.Kafka;
 
@@ -8,10 +9,22 @@ namespace Wwbweibo.CrackDetect.PerProcess.EventTrigger
 {
     public class KafkaMessageEventTrigger
     {
-        private KafkaClient client;
+        private static KafkaClient client;
 
-        public static void OnMessageTrigger()
+        public static void OnMessageTrigger(string server, string port, CancellationTokenSource cts)
         {
+            Task.Run(() =>
+            {
+                if (client == null)
+                {
+                    client = new KafkaClient(server, port);
+                    client.OnMessage += (sender, message) =>
+                    {
+                        // todo: here to get data and call worker to process
+                    };
+                    client.ListenMessage(new string[] { "PerProcessTopic"}, "PerProcessGroup", cts);
+                }
+            });
         }
 
         private static void Worker(int id, string b64Image)
--- Wwbweibo.CrackDetect.PerProcess/Program.cs
+++ Wwbweibo.CrackDetect.PerProcess/Program.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Wwbweibo.CrackDetect.PerProcess.EventTrigger;
 
 namespace Wwbweibo.CrackDetect.PerProcess
 {
@@ -13,7 +15,7 @@ namespace Wwbweibo.CrackDetect.PerProcess
     {
         public static void Main(string[] args)
         {
-            // 
+            KafkaMessageEventTrigger.OnMessageTrigger("ali.wwbweibo.me","9092", new CancellationTokenSource());
             CreateHostBuilder(args).Build().Run();
         }
 
