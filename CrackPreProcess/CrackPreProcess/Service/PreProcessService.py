235756a13c8ebeaad5a3e7148dc389b7695d45c0
wangweibo <wangweibo@beisen.com>
2020-01-03 10:36:05 +0800

    新增一些操作

--- CrackPreProcess/CrackPreProcess/Service/PreProcessService.py
+++ CrackPreProcess/CrackPreProcess/Service/PreProcessService.py
@@ -5,16 +5,16 @@ from CrackPreProcess.Utils.Utils import Decodeb64String, DecodeByte2Image
 流程待定
 '''
 class PreProcessService(object):
+    """description of class"""
     def __init__(self, base64string):
         self.imageb64String = base64string
+        self.image = None
         self.__decode_image__()
         
     def __decode_image__(self):
         bytedata = Decodeb64String(self.imageb64String)
         self.image = DecodeByte2Image(bytedata)
 
-    """description of class"""
-    def ExecuteWorkFlow(image):
-        pass
-    
+    def ExecuteWorkFlow(self):
+        return self.image.shape
   
--- CrackPreProcess/CrackPreProcess/views.py
+++ CrackPreProcess/CrackPreProcess/views.py
@@ -3,46 +3,24 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template
+from flask import render_template, request
 from CrackPreProcess import app
 from CrackPreProcess.Kafka  import Client
-from CrackPreProcess.Utils import Decodeb64String, DecodeByte2Image
 from CrackPreProcess.Service.PreProcessService import PreProcessService
 
 @app.route('/')
-@app.route('/home')
 def home():
-   
-    """Renders the home page."""
-    return render_template(
-        'index.html',
-        title='Home Page',
-        year=datetime.now().year,
-    )
+    return "anc"
 
-@app.route('/contact')
-def contact():
-    """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
-
-@app.route('/about')
-def about():
-    """Renders the about page."""
-    return render_template(
-        'about.html',
-        title='About',
-        year=datetime.now().year,
-        message='Your application description page.'
-    )
-
-@app.route("/perprocess", methods=['POST'])
-def PreProcess(imageb64data):
-    # basic validation
-    if imageb64data is None or len(imageb64data) <=0:
-        raise("the input data can not be None or empty")
-    service = PreProcessService(imageb64data)
+@app.route("/preprocess", methods=['POST'])
+def PreProcess():
+    try:
+        imageb64data = request.form['imageb64data']
+        # basic validation
+        if imageb64data is None or len(imageb64data) <=0:
+            raise("the input data can not be None or empty")
+        service = PreProcessService(imageb64data)
+        shape = service.ExecuteWorkFlow()
+        return '{%d,%d,%d}' % (shape[0], shape[1], shape[2])
+    except e:
+        print(e)
