9f75590dd3fcec2bc11be1d40229903fa3ad5b10
wangweibo <wangweibo@beisen.com>
2020-01-03 11:00:27 +0800

    图像分割

--- CrackPreProcess/CrackPreProcess/Service/PreProcessService.py
+++ CrackPreProcess/CrackPreProcess/Service/PreProcessService.py
@@ -14,7 +14,15 @@ class PreProcessService(object):
     def __decode_image__(self):
         bytedata = Decodeb64String(self.imageb64String)
         self.image = DecodeByte2Image(bytedata)
+        if(self.image.shape[0] != self.image.shape[1] != 1024):
+            raise("input image shape error, require 1024 * 1024 image")
 
     def ExecuteWorkFlow(self):
         return self.image.shape
-  
+
+    def CutImage():
+        im_list = []
+        for i in range(64):
+            for j in  range(64):
+                im_list.append(self.image[i * 16: (i+1) * 16, j * 16: (j+1) * 16, :])
+        self.image_block = im_list
