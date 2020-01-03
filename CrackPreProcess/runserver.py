ca8413558f9678b27bd65b103ff08d468714f3c4
wangweibo <wangweibo@beisen.com>
2020-01-03 10:42:19 +0800

    添加kafka消费者

--- CrackPreProcess/runserver.py
+++ CrackPreProcess/runserver.py
@@ -4,8 +4,17 @@ This script runs the CrackPreProcess application using a development server.
 
 from os import environ
 from CrackPreProcess import app
+from CrackPreProcess.Kafka.Client import Client
+from CrackPreProcess.Service.PreProcessService import PreProcessService
+
+def onMessage(message):
+    service = PreProcessService(message.value)
+    service.ExecuteWorkFlow()
 
 if __name__ == '__main__':
+    # when server started, start kafka consumer and listen the message
+    kafkaClent = Client("ali.wwbweibo.me", "9092")
+    Client.StartListenMessage("preprocess", onMessage, "python-preprocess")
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(environ.get('SERVER_PORT', '5555'))
