0b7a3bf0ac3d59399e20c45a2799993377b29a6c
wangweibo <wangweibo@beisen.com>
2020-01-06 16:30:27 +0800

    数据聚合网站

--- CrackPreProcess/CrackPreProcess/Service/PreProcessService.py
+++ CrackPreProcess/CrackPreProcess/Service/PreProcessService.py
@@ -1,80 +1,65 @@
 import cv2
-from CrackPreProcess.Utils.Utils import Decodeb64String, DecodeByte2Image
+from CrackPreProcess.Utils.Utils import Decodeb64String, DecodeByte2Image, EncodeData2b64
 from CrackPreProcess.Kafka.Client import Client
 import numpy as np
 
-'''
-流程待定
-'''
-class PreProcessService(object):
+class PreProcessService:
     """description of class"""
     def __init__(self, base64string):
         self.imageb64String = base64string
         self.image = None
+        self.client = None
         self.__decode_image__()
         
     def __decode_image__(self):
         bytedata = Decodeb64String(self.imageb64String)
         self.image = DecodeByte2Image(bytedata)
+
+    def execute_workflow(self):
         if(self.image.shape[0] != self.image.shape[1] != 1024):
-            raise("input image shape error, require 1024 * 1024 image")
+            raise Exception("input image shape error, require 1024 * 1024 image")
+        self.cut_image()
+        self.send_todo()
 
-    def ExecuteWorkFlow(self):
-        return self.image.shape
-
-    def CutImage(self):
+    def cut_image(self):
         im_list = []
         for i in range(64):
             for j in  range(64):
                 im_list.append(self.image[i * 16: (i+1) * 16, j * 16: (j+1) * 16, :])
         # conver the image blocks to a numpy array 
-        
+        im_list = np.asarray(im_list, dtype=np.uint8)
         self.image_block = im_list
+        self.serailized_image_block = im_list.tobytes()
 
     def convert_color_gray(self):
         """
         将bgr的图片转化为灰度图
-        :type image: opencv image
-        :param image: the image need to convert
-        :return: an image in gray color
         """
         self.gray_img = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
     
     def resize_img(self, width=1024):
         """
         缩放图像大小，默认为1024
-        :type img: image
-        :param img: input image
-        :type width: int
-        :param width: width after resize,800 as default
-        :return: image after resize
+        @param width: 图像缩放至的宽度
         """
         self.image = cv2.resize(self.image,(width, int(width * img.shape[0] / img.shape[1])))
         # 检查灰度图的大小
         if self.gray_img is not None or self.gray_img.shape[0] != self.gray_img[1] != 1024:
             self.gray_image = cv2.resize(self.gray_image,(width, int(width * img.shape[0] / img.shape[1])))
 
-    def convert_color(self,image, code=cv.COLOR_BGR2GRAY):
-        """
-        convert color space of an image
-        :type image: image
-        :param image: input image
-        :type code: opencv convert code
-        :param code: opencv color convert , COLOR_BGR2GRAY as default
-        :return: image after convert
-        """
-        return cv.cvtColor(image, code)
+    def resize_img1(self, width=1024, height=1024):
+        self.image = cv2.resize(self.image, (width, height))
+             
 
-    def center_avg_imp(img, ksize=10, flag=False):
+    def convert_color(self,image, code=cv2.COLOR_BGR2GRAY):
+        """
+        调整图像的色彩空间
+        """
+        return cv2.cvtColor(image, code)
+
+    def center_avg_imp(img, ksize=10):
         """
         improve the image pixels by image center pixel average
-        :type img: image
-        :param img: the image need to be improved
-        :type ksize: int
-        :param ksize: the filter size, 10 as default
-        :type flag: Boolean
-        :param flag: show the result or not
-        :return: the result after deal
         """
         new_img = np.copy(img)
 
@@ -99,78 +84,33 @@ class PreProcessService(object):
             for y in range(0, img.shape[1], ksize):
                 new_img[x:x + ksize, y:y + ksize] = \
                     img[x:x + ksize, y:y + ksize] * (avg / np.average(img[x:x + ksize, y:y + ksize]))
-        # img = cv.medianBlur(img, 3)
-        if flag:
-            plt.subplot(1, 2, 1)
-            plt.imshow(img, cmap='gray')
-            plt.subplot(1, 2, 2)
-            plt.imshow(new_img, cmap='gray')
-            plt.show()
-
         return new_img
 
     def equalize_hist(img, flag=False):
         """
-        equalize hist to improve image
-        :type img: image
-        :param img: input image
-        :type flag: Boolean
-        :param flag: show the result if is True, False as default
-        :return: the image after equalize hist
+        直方图均衡化
         """
         hist_img = np.zeros(shape=img.shape)
-        hist_img = cv.equalizeHist(img, hist_img)
-        if flag:
-            plt.subplot(2, 2, 1)
-            plt.imshow(img, cmap="gray")
-            plt.title("原图")
-            plt.subplot(2, 2, 2)
-            plt.hist(img)
-            plt.title("原图直方图")
-            plt.subplot(2, 2, 3)
-            plt.imshow(hist_img, cmap="gray")
-            plt.title("均衡化结果")
-            plt.subplot(2, 2, 4)
-            plt.hist(hist_img)
-            plt.title("均衡化结果直方图")
-            plt.show()
+        hist_img = cv2.equalizeHist(img, hist_img)
         return hist_img
 
 
-def med_blur(img, ksize=3, flag=False):
-    """
-    对图像进行中值滤波
-    :param img: input image
-    :param ksize: size of filter
-    :return: image after median filter
-    """
+    def med_blur(img, ksize=3, flag=False):
+        """
+        对图像进行中值滤波
+        """
+        if img.dtype is not np.uint8:
+            img = img.astype(np.uint8)
 
-    if img.dtype is not np.uint8:
-        img = img.astype(np.uint8)
-
-    new_img = cv.medianBlur(img, ksize)
-    if flag:
-        plt.subplot(2, 2, 1)
-        plt.imshow(img, cmap="gray")
-        plt.title("原图")
-        plt.subplot(2, 2, 2)
-        plt.hist(img)
-        plt.title("原图直方图")
-        plt.subplot(2, 2, 3)
-        plt.imshow(new_img, cmap="gray")
-        plt.title("中值滤波结果")
-        plt.subplot(2, 2, 4)
-        plt.hist(new_img)
-        plt.title("中值滤波结果直方图")
-        plt.show()
-    return new_img
+        new_img = cv2.medianBlur(img, ksize)
+        return new_img
 
 
     def gauss_blur(img, ksize=[3, 3]):
         """
         对图像进行高斯模糊
         """
-        cv.GaussianBlur(img, ksize=ksize)
+        cv2.GaussianBlur(img, ksize=ksize)
 
 
     def adj_gamma(img, flag=False):
@@ -183,15 +123,6 @@ def med_blur(img, ksize=3, flag=False):
         new_image = img
         new_image = new_image - np.min(np.min(new_image))
         new_image = new_image / np.max(np.max(new_image))
-
-        if flag:
-            x = np.arange(0, new_image.shape[1], 1)
-            y = np.arange(0, new_image.shape[0], 1)
-            xg, yg = np.meshgrid(x, y)
-            fig = plt.figure()
-            ax = Axes3D(fig)
-            ax.plot_surface(xg, yg, new_image, rstride=1, cstride=1, cmap=cm.viridis)
-            plt.show()
 
         return new_image
 
@@ -209,17 +140,9 @@ def med_blur(img, ksize=3, flag=False):
         xdata = np.linspace(1, pixel.shape[0], pixel.shape[0])
         index = np.argwhere(pixel == np.max(pixel))
         thresh = index[0][0] / 3
-        plt.plot(pixel)
-        plt.show()
 
-        ret, new_img = cv.threshold(img, thresh, 255, cv.THRESH_BINARY)
+        ret, new_img = cv2.threshold(img, thresh, 255, cv.THRESH_BINARY)
         new_img = np.abs(new_img - 255)
-        if flag:
-            plt.subplot(2, 1, 1)
-            plt.imshow(img, cmap="gray")
-            plt.subplot(2, 1, 2)
-            plt.imshow(new_img, cmap="gray")
-            plt.show()
         return new_img
 
 
@@ -227,18 +150,23 @@ def med_blur(img, ksize=3, flag=False):
         """
         对图像进行直方图分割
         """
-        hist = cv.calcHist([img], [0], None, [256], [0, 255])
+        hist = cv2.calcHist([img], [0], None, [256], [0, 255])
         max_index = np.where(hist == max(hist))
         mask = hist[0:max_index[0][0]]
         min_index = np.where(mask == min(mask))
-        ret, new_im = cv.threshold(img, min_index[0][0], 255, cv.THRESH_BINARY)
+        ret, new_im = cv2.threshold(img, min_index[0][0], 255, cv.THRESH_BINARY)
         return new_im
 
 
-    def sendTodo(self):
-        client = Client("ali.wwbweibo.me", "9092")
+    def send_todo(self):
+        if self.client is None:
+            self.client = Client("ali.wwbweibo.me", "9092")
+            self.client.sendMessage("calc-image", EncodeData2b64(self.serailized_image_block))
 
-        client.sendMessage("calc-image", )
-
-
-
+if __name__ == "__main__":
+    from CrackPreProcess.Utils import Utils
+    file = open("D:\\OneDrive\\Pictures\\47526064_p0.png.jpg", 'rb').read()
+    b64string = Utils.EncodeData2b64(file)
+    service = PreProcessService(b64string)
+    service.resize_img1()
+    service.execute_workflow()
