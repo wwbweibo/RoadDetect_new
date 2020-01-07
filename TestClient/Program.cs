8872164a61c1e8c26bfe0124800f4045014ed5bd
wangweibo <wangweibo@beisen.com>
2020-01-07 18:21:51 +0800

    添加测试客户端

---
+++ TestClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Wwbweibo.CrackDetect.Kafka;
+using System.IO;
+using Wwbweibo.CrackDetect.Tools.String;
+
+namespace TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var imagePath = @"D:\OneDrive\Pictures\47526064_p0.png.jpg";
+            var image = File.Open(imagePath, FileMode.Open, FileAccess.Read);
+            var buffer = new byte[image.Length];
+            image.Read(buffer, 0, (int)image.Length);
+            var b64Data = buffer.EncodeBytesToBase64String();
+            KafkaClient client = new KafkaClient("ali.wwbweibo.me","9092");
+            client.SendMessageAsync("preprocess", Guid.NewGuid().ToString()).GetAwaiter().GetResult();
+        }
+    }
+}
