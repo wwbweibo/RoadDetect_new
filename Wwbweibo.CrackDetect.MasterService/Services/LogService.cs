16810358a9f65969a4d0b7f5aa6113ae35c4f077
wwbweibo <wwbweibo@outlook.com>
2020-05-04 12:04:11 +0800

    添加日志页面

--- Wwbweibo.CrackDetect.MasterService/Services/LogService.cs
+++ Wwbweibo.CrackDetect.MasterService/Services/LogService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using Wwbweibo.CrackDetect.Libs.Redis;
 using Wwbweibo.CrackDetect.Models;
 
@@ -11,19 +12,23 @@ namespace Wwbweibo.CrackDetect.MasterService.Services
     {
         public IRedisClient redisClient { get; set; }
 
-        public LogModel[] GetAllLogMessages()
+        public Task<LogModel[]> GetAllLogMessages()
         {
-            var data = new List<string>();
-            foreach (int serviceType in Enum.GetValues(typeof(ServiceType)))
+            var t = Task.Run(() =>
             {
-                foreach (int logLevel in Enum.GetValues(typeof(LogLevel)))
+                var data = new List<string>();
+                foreach (int serviceType in Enum.GetValues(typeof(ServiceType)))
                 {
-                    data.AddRange(redisClient.LPop(serviceType.ToString() + logLevel.ToString()));
+                    foreach (int logLevel in Enum.GetValues(typeof(LogLevel)))
+                    {
+                        data.AddRange(redisClient.LPop(serviceType.ToString() + logLevel.ToString()));
+                    }
                 }
-            }
 
-            return data.Select(p => LogModel.Parser.ParseFrom(Encoding.UTF8.GetBytes(p))).OrderBy(p => p.LogTime).ToArray();
+                return data.Select(p => LogModel.Parser.ParseFrom(Encoding.UTF8.GetBytes(p))).OrderBy(p => p.LogTime).ToArray();
+            });
 
+            return t;
         }
 
         public LogModel[] GetSpecifyLog(int logLevel, int serviceType)
